4d429baf99ddc5be9ea98118c449f10ed9cf347b
doctor_ew <doctor_ew@protonmail.com>
2025-08-09 17:56:06 -0400

    tab icons

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,13 +6,29 @@ import { Pressable } from 'react-native';
 import Colors from '@/constants/Colors';
 import { useColorScheme } from '@/components/useColorScheme';
 import { useClientOnlyValue } from '@/components/useClientOnlyValue';
+import MaterialCommunityIcons from '@expo/vector-icons/build/MaterialCommunityIcons';
 
 // You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/
 function TabBarIcon(props: {
-  name: React.ComponentProps<typeof FontAwesome>['name'];
+  name: React.ComponentProps<typeof FontAwesome>['name'] | React.ComponentProps<typeof MaterialCommunityIcons>['name'];
   color: string;
+  iconLibrary?: 'FontAwesome' | 'MaterialCommunityIcons';
 }) {
-  return <FontAwesome size={28} style={{ marginBottom: -3 }} {...props} />;
+  const { iconLibrary, name, color } = props;
+  if (iconLibrary === 'MaterialCommunityIcons') {
+    return <MaterialCommunityIcons 
+      name={name as React.ComponentProps<typeof MaterialCommunityIcons>['name']} 
+      color={color} 
+      size={28} 
+      style={{ marginBottom: -3 }} />
+  }
+  if (iconLibrary === 'FontAwesome') {
+    return <FontAwesome 
+      name={name as React.ComponentProps<typeof FontAwesome>['name']} 
+      color={color} 
+      size={28} 
+      style={{ marginBottom: -3 }} />
+  }
 }
 
 export default function TabLayout() {
@@ -30,35 +46,35 @@ export default function TabLayout() {
         name="feed"
         options={{
           title: 'Feed',
-          tabBarIcon: ({ color }) => <TabBarIcon name="home" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="post-outline" color={color} iconLibrary='MaterialCommunityIcons' />,
         }}
       />
       <Tabs.Screen
         name="messages"
         options={{
           title: 'Messages',
-          tabBarIcon: ({ color }) => <TabBarIcon name="envelope" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="envelope" color={color} iconLibrary='FontAwesome' />,
         }}
       />
       <Tabs.Screen
         name="profile"
         options={{
           title: 'Profile',
-          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="user" color={color} iconLibrary='FontAwesome' />,
         }}
       />
       <Tabs.Screen
         name="search"
         options={{
           title: 'Search',
-          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="search" color={color} iconLibrary='FontAwesome' />,
         }}
       />
       <Tabs.Screen
         name="settings"
         options={{
           title: 'Settings',
-          tabBarIcon: ({ color }) => <TabBarIcon name="cog" color={color} />,
+          tabBarIcon: ({ color }) => <TabBarIcon name="cog" color={color} iconLibrary='FontAwesome' />,
         }}
       />
     </Tabs>
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -7,6 +7,7 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/components/useColorScheme';
+import React from 'react';
 
 export {
   // Catch any errors thrown by the Layout component.
